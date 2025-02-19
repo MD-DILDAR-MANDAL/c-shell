fd0b3bad89e49d72eb89d91adeccc61210b4a688
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-02-19 23:25:35 +0530

    feat: lsh launch

--- c-shell.c
+++ c-shell.c
@@ -110,3 +110,30 @@ char **lsh_split_line(char * line){
     tokens[position] = NULL;
     return tokens;
 }
+
+int lsh_launch(char **args){
+    pid_t pid, wpid;
+    int status;
+
+    /*https://www.digitalocean.com/community/tutorials/execvp-function-c-plus-plus
+    This is called the “fork-exec” model, 
+    and is the standard practice for running multiple processes using C.*/
+    pid = fork();
+    if(pid == 0){
+        //child process
+        if(execvp(args[0], args) == -1){
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    }else if(pid < 0){
+        //error forking
+        perror("lsh");
+    }else{
+        //parent process
+        do{
+            wpid = waitpid(pid, &status, WUNTRACED);
+        }while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
