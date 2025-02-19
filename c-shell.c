73ee42ce4d436a028729eac0cefa9d80d0f2451c
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-02-19 20:13:03 +0530

    feat: lsh split line function

--- c-shell.c
+++ c-shell.c
@@ -58,3 +58,55 @@ char * lsh_read_line(void){
         }
     }
 }
+
+/*
+//shortcut if you use getline() this does most of the work we just implemented above.
+
+char *lsh_read_line(void)
+{
+  char *line = NULL;
+  ssize_t bufsize = 0; // have getline allocate a buffer for us
+
+  if (getline(&line, &bufsize, stdin) == -1){
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS);  // We recieved an EOF
+    } else  {
+      perror("readline");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return line;
+}
+
+*/
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+char **lsh_split_line(char * line){
+    int bufsize = LSH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if(!tokens){
+        fprint(stderr,"lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token =  strtok(line, LSH_TOK_DELIM);
+    while(token != NULL){
+        tokens[position] = token;
+        position++;
+        if(position >= bufsize){
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens,bufsize * sizeof(char*));
+            if(!tokens){
+                fprintf(stderr,"lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
