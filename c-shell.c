ceabe9c335f2d9aaa8243c8baf3f3070f02e51de
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-03-03 00:02:31 +0530

    feat: add type

--- c-shell.c
+++ c-shell.c
@@ -203,11 +203,10 @@ int lsh_launch(char **args){
         perror("lsh");
     }else{
         //parent process
+     do{ 
         wpid = waitpid(pid, &status, WUNTRACED);
-    }while(!WIFEXITED(status) && !WIFSIGNALED(status));
-}
-do{
-
+        }while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
     return 1;
 }
 
@@ -236,30 +235,35 @@ So, it makes sense that we need to add some commands to the shell itself. The on
 /*function declarations for builtin shell commands:*/
 int lsh_cd(char **args);
 int lsh_help(char **args);
-int  lsh_exit(char **args);
+int lsh_exit(char **args);
+int lsh_type(char **args);
 
 /*list of builtin commands, followed by their corresponding functions.*/
 char *builtin_str [] = {
 	"cd",
 	"help",
-	"exit"
+	"exit",
+	"type"
 };
 
 int (*builtin_func[]) (char **) = {
 	&lsh_cd,
 	&lsh_help,
-	&lsh_exit
+	&lsh_exit,
+	&lsh_type
 }; 
 
 int lsh_num_builtins(){
 	return sizeof(builtin_str)/sizeof(char *);
 }
+
 /*Builtin function implementations*/
 int lsh_cd(char **args){
 	if(args[1] == NULL){
 		fprintf(stderr,"lsh: expected argument to \"cd\"\n");
-	}else{
-		if(chdir(args[1] != 0)){
+	}
+	else{
+		if(chdir(args[1]) != 0){
 			perror("lsh");
 		}
 	}
@@ -284,6 +288,21 @@ int lsh_exit(char **args){
 	return 0;
 }
 
+int lsh_type(char **args){
+	if(args[1] == NULL){
+		fprintf(stderr,"lsh: expected argument to \"type\" \n");	
+	}
+	else{
+		for(int i = 0;i < lsh_num_builtins();i++){
+            if(strcmp(args[1], builtin_str[i]) == 0){
+                printf("%s is a shell builtin \n",args[1]);
+                return 1;
+            }
+        }
+        printf("lsh: %s not found \n",args[1]);
+	}
+	return 1;
+}
 /*
 Putting together builtins and processes
 
